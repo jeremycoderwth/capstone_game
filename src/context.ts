a85a37c50005081f88927791ac5fd58ba1331ce5
jeremycoderwth <randeljeremy123@gmail.com>
2026-02-25 02:01:16 +0800

    Initialized context

---
+++ src/context.ts
@@ -0,0 +1,16 @@
+import kaplay from "kaplay";
+
+const k = kaplay({
+    width: 1300,
+    height: 600,
+    // add background if needed or requested
+    global: false,
+    debug: true,
+    touchToMouse: true,
+    pixelDensity: window.devicePixelRatio,
+    letterbox: true ,
+    font: "PressStart2P", // change to PressStart2P
+    root: document.getElementById("app") || undefined,
+});
+
+export default k;
